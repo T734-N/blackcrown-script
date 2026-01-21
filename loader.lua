5741bc9bb68edcf5c20c2e38b32ca433a152289a
T734-N <chaywachmniphechr@gmail.com>
2026-01-21 22:53:43 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,33 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/T734-N/blackcrown-script/refs/heads/main/main.lua"))()
+-- ==============================
+-- PLACE ID CONFIG
+-- ==============================
+
+local SCRIPT_BY_PLACE = {
+    [9412214742] = function()
+        -- Script ตัวที่ 1
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/T734-N/blackcrown-script/main/loader.lua"))()
+    end,
+
+    [9876543210] = function()
+        -- Script ตัวที่ 2
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/USER/REPO/main/script2.lua"
+        ))()
+    end
+}
+
+-- ==============================
+-- PLACE CHECK
+-- ==============================
+
+local runScript = SCRIPT_BY_PLACE[game.PlaceId]
+
+if not runScript then
+    warn("PlaceId นี้ไม่รองรับ")
+    return
+end
+
+-- ==============================
+-- RUN SCRIPT
+-- ==============================
+runScript()
