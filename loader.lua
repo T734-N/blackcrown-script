de979021d980ae074dfca6dcd707f16c4f44c5ed
T734-N <chaywachmniphechr@gmail.com>
2026-01-24 17:56:14 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,40 +1,15 @@
--- ==============================
--- PLACE ID CONFIG
--- ==============================
-local SCRIPT_BY_PLACE = {
-    [79311273910901] = "blade_spin",
-    [135016292071266] = "zombie_obby",
-    [97112073739947]  = "zombie_obby"
-}
+local placeId = game.PlaceId
 
-local SCRIPTS = {
-    blade_spin = function()
-        loadstring(game:HttpGet(
-            "https://raw.githubusercontent.com/T734-N/blackcrown-script/main/main.lua"
-        ))()
-    end,
+if placeId == 79311273910901 then
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/T734-N/blackcrown-script/main/main.lua"
+    ))()
 
-    zombie_obby = function()
-        loadstring(game:HttpGet(
-            "https://raw.githubusercontent.com/T734-N/blackcrown-script/main/obby%20Zombie.lua"
-        ))()
-    end
-}
+elseif placeId == 135016292071266 or placeId == 97112073739947 then
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/T734-N/blackcrown-script/main/obby%20Zombie.lua"
+    ))()
 
--- ==============================
--- PLACE CHECK
--- ==============================
-local scriptKey = SCRIPT_BY_PLACE[game.PlaceId]
-
-if not scriptKey then
-    warn("❌ PlaceId นี้ไม่รองรับ:", game.PlaceId)
-    return
+else
+    warn("PlaceId ไม่รองรับ:", placeId)
 end
-
-local scriptFunc = SCRIPTS[scriptKey]
-if not scriptFunc then
-    warn("❌ ไม่พบสคริปต์:", scriptKey)
-    return
-end
-
-scriptFunc()
