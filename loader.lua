6d41047cfa3f83442992781d42194987fee4a713
T734-N <chaywachmniphechr@gmail.com>
2026-01-24 17:29:39 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,25 +2,21 @@
 -- PLACE ID CONFIG
 -- ==============================
 local SCRIPT_BY_PLACE = {
-
-    -- สคริปต์เดียวกัน แต่รันได้หลาย PlaceId
     [79311273910901] = "blade spin",
-
-    -- อีกสคริปต์
     [135016292071266] = "zombie obby",
     [97112073739947] = "zombie obby"
 }
 
 local SCRIPTS = {
-    blade spin = function()
-        -- Script blade spin
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/T734-N/blackcrown-script/refs/heads/main/main.lua"))()
+    ["blade spin"] = function()
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/T734-N/blackcrown-script/main/main.lua"
+        ))()
     end,
 
-    zombie obby = function()
-        -- Script zombie obby
+    ["zombie obby"] = function()
         loadstring(game:HttpGet(
-            "https://raw.githubusercontent.com/T734-N/blackcrown-script/refs/heads/main/obby%20Zombie.lua"
+            "https://raw.githubusercontent.com/T734-N/blackcrown-script/main/obby%20Zombie.lua"
         ))()
     end
 }
@@ -28,11 +24,11 @@ local SCRIPTS = {
 -- ==============================
 -- PLACE CHECK
 -- ==============================
-
 local scriptKey = SCRIPT_BY_PLACE[game.PlaceId]
 if not scriptKey then
     warn("❌ PlaceId นี้ไม่รองรับ:", game.PlaceId)
     return
 end
 
+SCRIPTS[scriptKey]()
 SCRIPTS[scriptKey]()
